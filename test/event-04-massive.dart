b1960345c40579477584d55f280dbb943090f716
simonpai <simonpai@potix.com>
2014-05-22 22:08:32 +0800

    Add event test case.

---
+++ test/event-04-massive.dart
@@ -0,0 +1,24 @@
+import 'dart:html';
+import 'package:dquery/dquery.dart';
+
+void main() {
+  
+  $('#btn').on('click', render);
+  
+}
+
+void render(QueryEvent event) {
+  final Element root = new UListElement();
+  final Element container = document.querySelector('#list');
+  Element item;
+  
+  container.children.first.remove();
+  final int t = new DateTime.now().millisecondsSinceEpoch;
+  for (int i = 0; i < 1000; i++) {
+    item = new LIElement()..innerHtml = '$i';
+    $(item).on('click', (QueryEvent e) {});
+    root.append(item);
+  }
+  print(new DateTime.now().millisecondsSinceEpoch - t);
+  container.append(root);
+}
